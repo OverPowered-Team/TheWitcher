0556cb2464a33a6c8fb4deb86a3a82fb7c2553be
AxelAlavedra <axel.alavedra@gmail.com>
2020-03-17 16:25:27 +0100

    Creation of attack class

---
+++ The Witcher/CoreData/AlienEngineScripts/PlayerAttacks.cpp
@@ -0,0 +1,21 @@
+#include "PlayerAttacks.h"
+
+PlayerAttacks::PlayerAttacks() : Alien()
+{
+}
+
+PlayerAttacks::~PlayerAttacks()
+{
+}
+
+void PlayerAttacks::Start()
+{
+}
+
+void PlayerAttacks::Update()
+{
+}
+
+void PlayerAttacks::CreateAttacks()
+{
+}
---
+++ The Witcher/CoreData/AlienEngineScripts/PlayerAttacks.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "..\..\Alien Engine\Alien.h"
+#include "Macros/AlienScripts.h"
+
+class Attack {
+	std::string name = "";
+	std::string input = "";
+	float3 collider_position;
+	float3 collider_size;
+	float base_damage = 0.0f;
+
+	Attack* light_attack_link = nullptr;
+	Attack* heavy_attack_link = nullptr;
+};
+
+class ALIEN_ENGINE_API PlayerAttacks : public Alien {
+
+public:
+	PlayerAttacks();
+	virtual ~PlayerAttacks();
+	
+	void Start();
+	void Update();
+
+protected:
+	void CreateAttacks();
+
+protected:
+	Attack* current_attack = nullptr;
+	std::vector<Attack*> attacks;
+};
+
+ALIEN_FACTORY PlayerAttacks* CreatePlayerAttacks() {
+	PlayerAttacks* alien = new PlayerAttacks();
+	// To show in inspector here
+
+	return alien;
+} 
