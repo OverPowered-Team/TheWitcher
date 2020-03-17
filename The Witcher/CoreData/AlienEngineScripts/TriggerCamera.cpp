cc179bacca8044d4418825c6607693f99c71f71c
RoperoIvan <ivan_ropero98@hotmail.com>
2020-03-17 13:44:26 +0100

    The players now tp to a defined distance

--- The Witcher/CoreData/AlienEngineScripts/TriggerCamera.cpp
+++ The Witcher/CoreData/AlienEngineScripts/TriggerCamera.cpp
@@ -18,22 +18,28 @@ void TriggerCamera::Start()
 
 void TriggerCamera::Update()
 {
-	if (cam_script != nullptr && player_counter == cam_script->num_curr_players)
+	if (info_to_cam.tp_players && cam_script != nullptr && player_counter == 1)
 	{
-		//bool yes = ;
-		//if (yes)
-		//	cam_script->destination = cam_script->CalculateCameraPos(info_from_cam.hor_angle, info_from_cam.vert_angle, info_from_cam.distance);
-		//else
-		if (IsCameraDifferent())
-		{
-			cam_script->destination = cam_script->CalculateCameraPos(info_to_cam.hor_angle, info_to_cam.vert_angle, info_to_cam.distance);
-			InterChangeInfoWithCamera();
-			Tween::TweenMoveTo(camera, cam_script->destination, 2, Tween::linear);
-			cam_script->t1 = Time::GetGameTime();
-			cam_script->state = CameraMovement::CameraState::MOVING;
-		}
-		player_counter = 0;
+		ManageTransition();
+		TeleportTheRestOfPlayers();
 	}
+	else if (cam_script != nullptr && player_counter == cam_script->num_curr_players)
+	{
+		ManageTransition();
+	}
+}
+
+void TriggerCamera::ManageTransition()
+{
+	if (IsCameraDifferent())
+	{
+		cam_script->destination = cam_script->CalculateCameraPos(info_to_cam.hor_angle, info_to_cam.vert_angle, info_to_cam.distance);
+		InterChangeInfoWithCamera();
+		Tween::TweenMoveTo(camera, cam_script->destination, 2, Tween::linear);
+		cam_script->t1 = Time::GetGameTime();
+		cam_script->state = CameraMovement::CameraState::MOVING;
+	}
+	player_counter = 0;
 }
 
 void TriggerCamera::OnTriggerEnter(ComponentCollider* collider)
@@ -53,6 +59,8 @@ void TriggerCamera::OnTriggerEnter(ComponentCollider* collider)
 					
 		}
 	}
+	if (info_to_cam.tp_players)
+		info_to_cam.first_player = c->game_object_attached;
 }
 
 void TriggerCamera::OnTriggerExit(ComponentCollider* collider)
@@ -60,14 +68,15 @@ void TriggerCamera::OnTriggerExit(ComponentCollider* collider)
 	Component* c = (Component*)collider;
 	if (player_counter >= 0)
 	{
-		if (player_counter == 0)
-			player_counter = cam_script->num_curr_players;
+		/*if (player_counter == 0)
+			player_counter = cam_script->num_curr_players;*/
 		for (std::map<GameObject*, bool>::iterator it = cam_script->players.begin(); it != cam_script->players.end(); ++it)
 		{
 			if (it->first == c->game_object_attached)
 			{
 				it->second = false;
-				player_counter--;
+				if(player_counter > 0)
+					player_counter--;
 				LOG("PLAYER EXIT");
 			}
 
@@ -89,20 +98,29 @@ void TriggerCamera::InterChangeInfoWithCamera()
 	cam_script->distance = info_to_cam.distance;
 }
 
+void TriggerCamera::TeleportTheRestOfPlayers()
+{
+	float3 first_player_pos = info_to_cam.first_player->transform->GetGlobalPosition();
+	GameObject** get_players = nullptr;
+	uint size;
+	size = GameObject::FindGameObjectsWithTag("Player", &get_players);
+	for (int i = 0; i < size; i++) {
+		GameObject* g = get_players[i];
+		if (g != info_to_cam.first_player)
+		{
+			float3 dist(info_to_cam.tp_distance, 0, 0); //Delete this info?
+			g->transform->SetGlobalPosition(first_player_pos + dist);
+		}
+	}
+	info_to_cam.first_player = nullptr;
+}
+
 bool TriggerCamera::IsCameraDifferent()
 {
-	//if (is_backing)
-	//{
-	//	cam_script->top_angle = info_from_cam.hor_angle;
-	//	cam_script->vertical_angle = info_from_cam.vert_angle;
-	//	cam_script->distance = info_from_cam.distance;
-	//	is_backing = false;
-	//}
 	if (info_to_cam.hor_angle == cam_script->top_angle &&
 		info_to_cam.vert_angle == cam_script->vertical_angle &&
 		info_to_cam.distance == cam_script->distance)
 	{
-		//is_backing = true;
 		return false;
 	}
 	return true;
--- The Witcher/CoreData/AlienEngineScripts/TriggerCamera.h
+++ The Witcher/CoreData/AlienEngineScripts/TriggerCamera.h
@@ -8,18 +8,24 @@ class ALIEN_ENGINE_API TriggerCamera : public Alien {
 public:
 	struct InfoCamera
 	{
+		GameObject* first_player = nullptr;
 		float distance = 0.f;
 		float hor_angle = 0.f;
 		float vert_angle = 0.f;
+		bool tp_players = false;
+		float tp_distance = 0.f;
 	};
 	TriggerCamera();
 	virtual ~TriggerCamera();
 	void Start();
 	void Update();
+	void ManageTransition();
 	void OnTriggerEnter(ComponentCollider* collider);
 	void OnTriggerExit(ComponentCollider* collider);
 	void InterChangeInfoWithCamera();
+	void TeleportTheRestOfPlayers();
 	bool IsCameraDifferent();
+
 public:
 	InfoCamera info_to_cam;
 	InfoCamera info_from_cam;
@@ -37,6 +43,7 @@ ALIEN_FACTORY TriggerCamera* CreateTriggerCamera() {
 	SHOW_IN_INSPECTOR_AS_DRAGABLE_FLOAT(alien->info_to_cam.distance);
 	SHOW_IN_INSPECTOR_AS_DRAGABLE_FLOAT(alien->info_to_cam.hor_angle, -360.f, 360.f);
 	SHOW_IN_INSPECTOR_AS_DRAGABLE_FLOAT(alien->info_to_cam.vert_angle, -360.f, 360.f);
-
+	SHOW_IN_INSPECTOR_AS_CHECKBOX_BOOL(alien->info_to_cam.tp_players);
+	SHOW_IN_INSPECTOR_AS_DRAGABLE_FLOAT(alien->info_to_cam.tp_distance);
 	return alien;
 }
