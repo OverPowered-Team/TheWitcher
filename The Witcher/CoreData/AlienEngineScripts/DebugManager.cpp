e04a47e19d23d316c68c564a2a0dc596fb06b420
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2020-04-02 10:35:02 +0200

    Debug Manager

---
+++ The Witcher/CoreData/AlienEngineScripts/DebugManager.cpp
@@ -0,0 +1,33 @@
+#include "DebugManager.h"
+
+DebugManager::DebugManager() : Alien()
+{
+}
+
+DebugManager::~DebugManager()
+{
+}
+
+void DebugManager::Start()
+{
+}
+
+void DebugManager::Update()
+{
+	if (Input::GetKeyRepeat(SDL_SCANCODE_LALT))
+	{
+		if (Input::GetKeyDown(SDL_SCANCODE_M))
+		{
+			SceneManager::LoadScene("Main_Menu");
+		}
+		if (Input::GetKeyDown(SDL_SCANCODE_W))
+		{
+			SceneManager::LoadScene("Win");
+		}
+		if (Input::GetKeyDown(SDL_SCANCODE_L))
+		{
+			SceneManager::LoadScene("Lose");
+		}
+	}
+
+}
