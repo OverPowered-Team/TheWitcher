f77d75f3820cdbbffb2e3b37fcc6c44ecbf0987d
Christian Martínez <christt105@gmail.com>
2020-04-02 14:58:24 +0200

    breakable fence XD

--- The Witcher/CoreData/AlienEngineScripts/BreakableObject.cpp
+++ The Witcher/CoreData/AlienEngineScripts/BreakableObject.cpp
@@ -19,6 +19,15 @@ void BreakableObject::Update()
 void BreakableObject::OnTriggerEnter(ComponentCollider* collider)
 {
 	if (strcmp("PlayerAttack", collider->game_object_attached->GetTag()) == 0) {
+		GameObject* new_obj = GameObject::Instantiate(object_broken, transform->GetGlobalPosition(), game_object->parent);
+		new_obj->transform->SetGlobalRotation(transform->GetGlobalRotation());
+		((ComponentRigidBody*)new_obj->GetComponent(ComponentType::RIGID_BODY))->AddTorque(-float3::unitZ());
+
+		//std::vector<GameObject*> children = new_obj->GetChildren();
+		/*for (auto i = children.begin(); i != children.end(); ++i) {
+			(*i).
+		}*/
+
 		GameObject::Destroy(game_object);
 	}
 }
