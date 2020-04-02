d07ab09e1004bbc462053e54d71da7649d0a4ff6
RoperoIvan <ivan_ropero98@hotmail.com>
2020-04-02 23:52:38 +0200

    Scene changed

---
+++ The Witcher/CoreData/AlienEngineScripts/Trigger_Win.cpp
@@ -0,0 +1,17 @@
+#include "Trigger_Win.h"
+
+Trigger_Win::Trigger_Win() : Alien()
+{
+}
+
+Trigger_Win::~Trigger_Win()
+{
+}
+
+void Trigger_Win::Start()
+{
+}
+
+void Trigger_Win::Update()
+{
+}
---
+++ The Witcher/CoreData/AlienEngineScripts/Trigger_Win.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "..\..\Alien Engine\Alien.h"
+#include "Macros/AlienScripts.h"
+
+class ALIEN_ENGINE_API Trigger_Win : public Alien {
+
+public:
+
+	Trigger_Win();
+	virtual ~Trigger_Win();
+	
+	void Start();
+	void Update();
+
+};
+
+ALIEN_FACTORY Trigger_Win* CreateTrigger_Win() {
+	Trigger_Win* alien = new Trigger_Win();
+	// To show in inspector here
+
+	return alien;
+} 
