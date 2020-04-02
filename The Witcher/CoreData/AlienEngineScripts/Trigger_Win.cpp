d12e99a247978d7420c486ad80b9768b5b9de7a9
Alex Morales Garcia <36154498+AlexMG99@users.noreply.github.com>
2020-04-03 00:24:12 +0200

    Merge branch 'dev' of https://github.com/OverPowered-Team/TheWitcher into dev

--- The Witcher/CoreData/AlienEngineScripts/Trigger_Win.cpp
+++ The Witcher/CoreData/AlienEngineScripts/Trigger_Win.cpp
@@ -15,3 +15,17 @@ void Trigger_Win::Start()
 void Trigger_Win::Update()
 {
 }
+
+void Trigger_Win::OnTriggerEnter(ComponentCollider* collider)
+{
+	ComponentAudioEmitter* em = (ComponentAudioEmitter*)collider->game_object_attached->GetComponent(ComponentType::A_EMITTER);
+	if (Time::GetGameTime() - timer >= 2.f)
+	{
+		if (em != nullptr && !first)
+		{
+			SceneManager::LoadScene("Win");
+			first = true;
+		}
+	}
+	
+}
