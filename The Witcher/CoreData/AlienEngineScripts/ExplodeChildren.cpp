9b9ee4c869df6b6981d87a68b84577d1652b3809
Alex Morales Garcia <36154498+AlexMG99@users.noreply.github.com>
2020-04-03 02:56:17 +0200

    Prefab changed

--- The Witcher/CoreData/AlienEngineScripts/ExplodeChildren.cpp
+++ The Witcher/CoreData/AlienEngineScripts/ExplodeChildren.cpp
@@ -14,7 +14,7 @@ void ExplodeChildren::Start()
 
 	uint size = game_object->GetComponentsInChildren(ComponentType::RIGID_BODY, (Component***)&rbs, false);
 	for (auto i = 0u; i < size; ++i) {
-		rbs[i]->AddForce(float3(Random::GetRandomIntBetweenTwo(-50, 50), Random::GetRandomIntBetweenTwo(0, 30), Random::GetRandomIntBetweenTwo(-50, 50)).Normalized() * 10);
+		rbs[i]->AddForce(float3(Random::GetRandomIntBetweenTwo(-50, 50), Random::GetRandomIntBetweenTwo(0, 30), Random::GetRandomIntBetweenTwo(-50, 50)).Normalized() * 3.0);
 	}
 
 	GameObject::FreeArrayMemory((void***)&rbs);
