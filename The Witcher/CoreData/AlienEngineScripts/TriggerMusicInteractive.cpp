836a74bfcf674f916a17698f7b89db5f4bfec6ca
GerardClotet <36155703+GerardClotet@users.noreply.github.com>
2020-04-22 20:10:01 +0200

    geralt combat improve

--- The Witcher/CoreData/AlienEngineScripts/TriggerMusicInteractive.cpp
+++ The Witcher/CoreData/AlienEngineScripts/TriggerMusicInteractive.cpp
@@ -1,6 +1,7 @@
 #include "TriggerMusicInteractive.h"
 #include "PlayerController.h"
 #include "CameraMovement.h"
+#include "MusicController.h"
 
 TriggerMusicInteractive::TriggerMusicInteractive() : Alien()
 {
@@ -15,19 +16,17 @@ void TriggerMusicInteractive::Start()
 	camera = Camera::GetCurrentCamera()->game_object_attached;
 	cam_script = camera->GetComponent<CameraMovement>();
 	timer = Time::GetGameTime();
-	emitter = game_object->GetComponent<ComponentAudioEmitter>();
-	emitter->SetState("Interactive_Music_Lvl1", "Quiet");
-	emitter->ChangeVolume(0.f);
-	emitter->StartSound();
+	emitter = camera->GetComponent<ComponentAudioEmitter>();
+	m_controller = camera->GetComponent<MusicController>();
 }
 
 void TriggerMusicInteractive::Update()
 {
-	if (first_time && Time::GetGameTime() - timer >= 1.f)
-	{
-		emitter->ChangeVolume(0.5f);
-		first_time = false;
-	}
+	//if (first_time && Time::GetGameTime() - timer >= 1.f)
+	//{
+	//	emitter->ChangeVolume(0.5f);
+	//	first_time = false;
+	//}
 }
 
 void TriggerMusicInteractive::OnTriggerEnter(ComponentCollider* collider)
@@ -38,9 +37,11 @@ void TriggerMusicInteractive::OnTriggerEnter(ComponentCollider* collider)
 		if (strcmp(collider->game_object_attached->GetTag(), "Player") == 0)
 		{
 			emitter->SetState("Interactive_Music_Lvl1", GetNameByEnum(interactive).c_str());
+			m_controller->last_music = GetNameByEnum(interactive).c_str();
+			m_controller->has_changed = true;
 		}
-		
-	}	
+
+	}
 }
 
 std::string TriggerMusicInteractive::GetNameByEnum(Music mat)
