cd20ab028f562f17d16f853b885177899f56ea7c
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2020-04-28 12:58:42 +0200

    Controls Effect when ulti is ready

--- The Witcher/CoreData/AlienEngineScripts/UltiBar.h
+++ The Witcher/CoreData/AlienEngineScripts/UltiBar.h
@@ -11,21 +11,36 @@ public:
 	virtual ~UltiBar();
 	
 	void Start();
+	void Update();
 
 	void UpdateBar(float actual_value);
 	void MaxBar();
 
+	void ControlsLerp();
+
 	GameObject* ultibar = nullptr;
 	GameObject* ultibar_charged = nullptr;
-	
+	GameObject* controls = nullptr;
+	GameObject* controls_lb = nullptr;
+	GameObject* controls_rb = nullptr;
 	ComponentBar* normal_ulti = nullptr;
 	ComponentBar* charged_ulti = nullptr;
 
+	float time_to_lerp = 0.5f;
+	int times_shine = 3;
+
+private:
+	int shining_count = 0;
+	float actual_time = 0.0f;
+	bool controls_lerping = false;
 };
 
 ALIEN_FACTORY UltiBar* CreateUltiBar() {
 	UltiBar* alien = new UltiBar();
 	// To show in inspector here
+
+	SHOW_IN_INSPECTOR_AS_INPUT_FLOAT(alien->time_to_lerp);
+	SHOW_IN_INSPECTOR_AS_INPUT_INT(alien->times_shine);
 
 	return alien;
 } 
