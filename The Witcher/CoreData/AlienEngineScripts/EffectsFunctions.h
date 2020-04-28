3432691a14949c369130ef0b0cb0893a80c9ee3c
CereVenteo <36203330+CereVenteo@users.noreply.github.com>
2020-04-28 14:59:53 +0200

    relics attacks

--- The Witcher/CoreData/AlienEngineScripts/EffectsFunctions.h
+++ The Witcher/CoreData/AlienEngineScripts/EffectsFunctions.h
@@ -11,49 +11,30 @@ static void ApplyEffectOnHit(Enemy* _enemy, uint size, EffectData* data)
 {
     Effect* effect = new Effect();
     effect->name = data->name;
-    effect->time = size * data->time;
+    if(strcmp(data->name.data(), "poison_runestone") == 0)
+        effect->time = data->time;
+    else
+        effect->time = size * data->time;
     effect->ticks_time = data->ticks_time;
     effect->last_tick_time = Time::GetGameTime();
     effect->start_time = Time::GetGameTime();
     
     for (int i = 0; i < data->additive_modifiers.size(); ++i)
     {
-        effect->AddFlatModifier(data->additive_modifiers[i].amount, data->additive_modifiers[i].identifier);
+        if (strcmp(data->name.data(), "poison_runestone") == 0)
+            effect->AddFlatModifier(data->additive_modifiers[i].amount * size, data->additive_modifiers[i].identifier);
+        else
+            effect->AddFlatModifier(data->additive_modifiers[i].amount, data->additive_modifiers[i].identifier);
     }
     for (int i = 0; i < data->multiplicative_modifiers.size(); ++i)
     {
         effect->AddMultiplicativeModifier(data->multiplicative_modifiers[i].amount, data->multiplicative_modifiers[i].identifier);
     }
-
-    _enemy->AddEffect(effect);
+    if (strcmp(data->name.data(), "lightning_runestone") == 0)
+        _enemy->AddEffect(effect);
+    else
+        _enemy->AddEffect(effect);
 }
-
-//static void ApplyIceOnHit(Enemy* _enemy, uint size, Effect* effect)
-//{
-//    effect->AddMultiplicativeModifier(effect->valor, "Agility");
-//    effect->name = "Ice On Hit";
-//    effect->time = size * effect->time;
-//    effect->ticks_time = effect->ticks_time;
-//    effect->last_tick_time = Time::GetGameTime();
-//    effect->start_time = Time::GetGameTime();
-//    _enemy->AddEffect(effect);
-//}
-//
-//static void ApplyLightningOnHit(Enemy* _enemy, uint size, Effect* effect)
-//{
-//    
-//}
-//
-//static void ApplyPoisonOnHit(Enemy* _enemy, uint size, Effect* effect)
-//{
-//    effect->AddFlatModifier(size * effect->valor, "Health");
-//    effect->name = "Poison On Hit";
-//    effect->time = effect->time;
-//    effect->ticks_time = effect->ticks_time;
-//    effect->last_tick_time = Time::GetGameTime();
-//    effect->start_time = Time::GetGameTime();
-//    _enemy->AddEffect(effect);
-//}
 
 //ONDASH
 static void ApplyBurnOnDash(PlayerController* _player)
