98b952632e57ccd0dbab2c590d3f3223becfb0e9
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2020-04-29 11:50:04 +0200

    Ultibar now works with lerp

--- The Witcher/CoreData/AlienEngineScripts/UltiBar.cpp
+++ The Witcher/CoreData/AlienEngineScripts/UltiBar.cpp
@@ -10,31 +10,41 @@ UltiBar::~UltiBar()
 
 void UltiBar::Start()
 {
+	// GameObjects
 	ultibar = game_object->GetChild("Ultibar");
 	ultibar_charged = game_object->GetChild("Ultibar_Charged");
+	normal_ulti = ultibar->GetComponent<ComponentBar>();
 	ultimate_bar = ultibar_charged->GetComponent<ComponentBar>();
+
+	// Controls
 	controls = game_object->GetChild("Ulti_Controls");
 	controls_lb = controls->GetChild("LeftControl");
 	controls_rb = controls->GetChild("RightControl");
-	pre_scale = controls_lb->transform->GetLocalScale().x;
 	lb_image = controls_lb->GetComponent<ComponentImage>();
 	rb_image = controls_rb->GetComponent<ComponentImage>();
+	pre_scale = controls_lb->transform->GetLocalScale().x;
+
+	// Enables Setting
 	controls->SetEnable(false);
-	ultibar->SetEnable(true);
-	normal_ulti = ultibar->GetComponent<ComponentBar>();
-	normal_ulti->SetBarValue(0);
 	ultibar_charged->SetEnable(false);
+	ultibar->SetEnable(true);
+	normal_ulti->SetBarValue(0);
 }
 
 void UltiBar::Update()
 {
-	if (Input::GetKeyDown(SDL_SCANCODE_SPACE))
+	// Bar Charges With Lerp
+	if (bar_charging)
 	{
-		controls->SetEnable(true);
-		actual_time = Time::GetGameTime();
-		glow_time = Time::GetGameTime();
-		controls_lerping = true;
-		ultibar_charged->SetEnable(true);
+		float t = (Time::GetGameTime() - bar_charging_time) * (1 / ulti_increase_lerp_time);
+		float lerp = Maths::Lerp(previous_bar_value, new_bar_value, t);
+
+		normal_ulti->SetBarValue(lerp);
+
+		if (t >= 1)
+		{
+			bar_charging = false;
+		}
 	}
 
 	if (controls_lerping)
@@ -42,6 +52,7 @@ void UltiBar::Update()
 		ControlsLerp();
 	}
 
+	// Charged Bar Glowing
 	if (ultibar_charged->IsEnabled())
 	{
 		float t = (Time::GetGameTime() - glow_time);
@@ -68,32 +79,41 @@ void UltiBar::Update()
 
 void UltiBar::UpdateBar(float actual_value)
 {
-	normal_ulti->SetBarValue(actual_value);
 	if (actual_value == 0)
 	{
 		ultibar->SetEnable(true);
 		ultibar_charged->SetEnable(false);
 	}
+
+	previous_bar_value = normal_ulti->GetBarValue();
+	new_bar_value = actual_value;
+	bar_charging = true;
+	bar_charging_time = Time::GetGameTime();
 }
 
 void UltiBar::MaxBar()
 {
-	controls->SetEnable(true);
-	normal_ulti->SetBarValue(1);
+	// Enables Setting
 	ultibar->SetEnable(false);
+	controls->SetEnable(true);
 	ultibar_charged->SetEnable(true);
+
+	// Values Setting
+	normal_ulti->SetBarValue(1);
 	controls_lerping = true;
+
+	// Lerping Time Settings
 	glow_time = Time::GetGameTime();
 	actual_time = Time::GetGameTime();
 }
 
 void UltiBar::ControlsLerp()
 {
-	float t = (Time::GetGameTime() - actual_time) * (1 / time_to_lerp);
+	float t = (Time::GetGameTime() - actual_time) * (1 / time_to_lerp_controls);
 	float scale = 0.0f;
 	float color = 0.f;
 
-	if ((Time::GetGameTime() - actual_time) < (time_to_lerp * 0.5f))
+	if ((Time::GetGameTime() - actual_time) < (time_to_lerp_controls * 0.5f))
 	{
 		scale = Maths::Lerp(pre_scale, (pre_scale * (1.f + 1.0f / 3.0f)), t);
 		color = Maths::Lerp(0.275f, 0.85f, t);
@@ -111,8 +131,8 @@ void UltiBar::ControlsLerp()
 
 	if (t >= 1)
 	{
-		actual_time = Time::GetGameTime();
 		++shining_count;
+		actual_time = Time::GetGameTime();
 
 		if (shining_count >= times_shine)
 		{
