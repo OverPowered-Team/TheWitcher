36b835db5a7f44af4bb560c2e9217f1fac01305c
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2020-05-04 19:13:49 +0200

    trying to do rotations

--- The Witcher/CoreData/AlienEngineScripts/VagoneteMove.cpp
+++ The Witcher/CoreData/AlienEngineScripts/VagoneteMove.cpp
@@ -7,17 +7,22 @@ void VagoneteMove::Start()
 
 void VagoneteMove::Update()
 {
-	static float actual_pos = 0;
-
-	transform->SetLocalPosition(curve->curve.ValueAt(actual_pos));
-
+	float3 currentPos = curve->curve.ValueAt(actual_pos);
+	float3 nextPos = curve->curve.ValueAt(actual_pos + ((1 / (float)curve->curve.detail) * Time::GetDT() * speed) * 10);
+	
+	float3 vector = (nextPos - currentPos).Normalized();
+	float angle = atan2f(vector.y, vector.x);
+	LOG("%f", angle);
 	Quat myRot = transform->GetLocalRotation();
 	float3 normal = curve->curve.NormalAt(actual_pos);
+	Quat myDesiredRotUp = Quat::RotateFromTo(myRot.WorldY(), normal);
+	Quat myDesiredRotForward = Quat::RotateZ(angle);
 
-	Quat myDesiredRotUp = Quat::RotateFromTo(transform->up, normal);
+	// myDesiredRotForward
+	// myDesiredRotUp * myRot
 
-	transform->SetLocalRotation(myDesiredRotUp * myRot);
-
+	transform->SetGlobalRotation(myDesiredRotUp * myDesiredRotForward);
+	transform->SetLocalPosition(currentPos);
 
 	actual_pos += (1/(float)curve->curve.detail) * Time::GetDT() * speed;
 
