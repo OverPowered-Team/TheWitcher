53d5063307c9318b924a4b987722284be668d074
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2020-05-04 20:58:54 +0200

    Update VagoneteMove.cpp

--- The Witcher/CoreData/AlienEngineScripts/VagoneteMove.cpp
+++ The Witcher/CoreData/AlienEngineScripts/VagoneteMove.cpp
@@ -11,14 +11,13 @@ void VagoneteMove::Update()
 	float3 nextPos = curve->curve.ValueAt(actual_pos + ((1 / (float)curve->curve.detail) * Time::GetDT() * speed) * 10);
 	
 	float3 vector = (nextPos - currentPos).Normalized();
-	float angle = atan2f(vector.y, vector.x);
 
-	float3 normal = curve->curve.NormalAt(actual_pos);
+	float3 normal = curve->curve.NormalAt(actual_pos).Normalized();
+	float3 Y = vector.Cross(normal);
 
-	Quat myDesiredRotUp = Quat::RotateFromTo(Quat::identity().WorldY(), normal);
-	Quat myDesiredRotForward = Quat::RotateZ(angle);
+	float3x3 rot = float3x3(vector, normal, Y);
 
-	transform->SetLocalRotation(myDesiredRotForward * myDesiredRotUp);
+	transform->SetLocalRotation(rot.ToQuat());
 	transform->SetLocalPosition(currentPos);
 
 	actual_pos += (1/(float)curve->curve.detail) * Time::GetDT() * speed;
