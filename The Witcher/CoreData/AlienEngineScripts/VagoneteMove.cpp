1ec1f82e8ce9614b7482d81e4903f20f816bcc24
Yessica Servin Dominguez <36170847+YessicaSD@users.noreply.github.com>
2020-05-04 20:22:52 +0200

    changed calcul angle

--- The Witcher/CoreData/AlienEngineScripts/VagoneteMove.cpp
+++ The Witcher/CoreData/AlienEngineScripts/VagoneteMove.cpp
@@ -11,7 +11,7 @@ void VagoneteMove::Update()
 	float3 nextPos = curve->curve.ValueAt(actual_pos + ((1 / (float)curve->curve.detail) * Time::GetDT() * speed) * 10);
 	
 	float3 vector = (nextPos - currentPos).Normalized();
-	float angle = atan2f(vector.y, vector.x);
+	float angle = atan2f(vector.y, math::Sqrt((vector.z * vector.z) + (vector.x * vector.x)));
 
 	float3 normal = curve->curve.NormalAt(actual_pos);
 
