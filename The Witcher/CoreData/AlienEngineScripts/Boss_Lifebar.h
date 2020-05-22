2f42929f457b47709fa004e8202e1c8898bbfece
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2020-05-22 10:47:00 +0200

    Boss lifebar Logic

---
+++ The Witcher/CoreData/AlienEngineScripts/Boss_Lifebar.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "..\..\Alien Engine\Alien.h"
+#include "Macros/AlienScripts.h"
+
+class ALIEN_ENGINE_API Boss_Lifebar : public Alien {
+
+public:
+
+	Boss_Lifebar();
+	virtual ~Boss_Lifebar();
+	
+private:
+	void Start();
+	void Update();
+
+public:
+
+	void UpdateLifebar(float life, float max_life);
+
+	void ShowLifeBar(bool want_to_show);
+
+public:
+
+	float time_to_lerp			= 0.25f;
+	float time_to_show			= 0.5f;
+
+private:
+
+	// Components
+	ComponentBar* lifebar		= nullptr;
+	ComponentText* boss_name	= nullptr;
+
+	// Effects
+	bool lifebar_changing		= false;
+	float start_lerp_time		= 0.0f;
+	float goal_life				= 0.0f;
+	float start_life			= 0.0f;
+
+	// Showing Effect
+	bool changing_alpha			= false;
+	float start_time_show		= false;
+	float starting_point		= false;
+	float ending_point			= false;
+};
+
+ALIEN_FACTORY Boss_Lifebar* CreateBoss_Lifebar() {
+	Boss_Lifebar* alien = new Boss_Lifebar();
+	// To show in inspector here
+
+	SHOW_IN_INSPECTOR_AS_INPUT_FLOAT(alien->time_to_lerp);
+	SHOW_IN_INSPECTOR_AS_INPUT_FLOAT(alien->time_to_show);
+
+	return alien;
+} 
