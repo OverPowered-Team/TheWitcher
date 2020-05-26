5ed403e211c8e529b58429c374681504563b1994
Yessica Servin Dominguez <36170847+YessicaSD@users.noreply.github.com>
2020-05-26 13:54:17 +0200

    move camera with lerp

--- The Witcher/CoreData/AlienEngineScripts/MoveCameraArroundObject.cpp
+++ The Witcher/CoreData/AlienEngineScripts/MoveCameraArroundObject.cpp
@@ -10,27 +10,55 @@ MoveCameraArroundObject::~MoveCameraArroundObject()
 
 void MoveCameraArroundObject::Start()
 {
+	start_pos = game_object->transform->GetGlobalPosition();
 	if (path)
 	{
 		curve = path->GetComponent<ComponentCurve>();
-		if(curve!=nullptr)
+		if (curve != nullptr)
+		{
 			LOG("Curve found!");
+			camera = GetComponent<ComponentCamera>();
+			start_curve = curve->curve.ValueAt(current_pos);
+		}
+
 	}
 }
 
 void MoveCameraArroundObject::Update()
 {
-	if (curve)
+	if (curve && camera)
 	{
-		float3 currentPos = curve->curve.ValueAt(current_pos);
-		LOG("%f", current_pos);
-		float3 nextPos = curve->curve.ValueAt(current_pos + speed * Time::GetDT() * 5);
-		float3 railVector = (currentPos - nextPos).Normalized();
-		game_object->transform->SetGlobalPosition(currentPos);
-		current_pos += speed * Time::GetDT();
-		if (current_pos > 1)
+		float3 currentPos = target->transform->GetGlobalPosition();
+
+		if (start_transition)
 		{
-			current_pos = 0;
+			current_time_transition += Time::GetDT();
+			if (current_time_transition >= transition_duration)
+			{
+				currentPos = math::Lerp(start_pos, start_curve, 1);
+				start_transition = false;
+			}
+			else
+			{
+				float percentatge_lerp = current_time_transition / transition_duration;
+				currentPos = math::Lerp(start_pos, start_curve, percentatge_lerp);
+			}
 		}
+		else
+		{
+			currentPos = curve->curve.ValueAt(current_pos);
+			current_pos += speed * Time::GetDT();
+			if (current_pos > 1)
+			{
+				current_pos = 0;
+			}
+		}
+
+			game_object->transform->SetGlobalPosition(currentPos);
+			float3 front_vector = target->transform->GetGlobalPosition() - currentPos;
+			front_vector.Normalize();
+			camera->frustum.front = front_vector;
+			camera->frustum.up = float3::unitY();
+		
 	}
 }
--- The Witcher/CoreData/AlienEngineScripts/MoveCameraArroundObject.h
+++ The Witcher/CoreData/AlienEngineScripts/MoveCameraArroundObject.h
@@ -6,10 +6,18 @@
 class ALIEN_ENGINE_API MoveCameraArroundObject : public Alien {
 
 public:
-	GameObject* path;
+	GameObject* path = nullptr;
+	GameObject* target = nullptr;
 	ComponentCurve* curve = nullptr;
+	ComponentCamera* camera = nullptr;
+	float3 start_pos = float3::zero();
+	float3 start_curve = float3::zero();
+
 	float speed = 0;
 	float current_pos = 0; 
+	bool start_transition = true;
+	float transition_duration = 0;
+	float current_time_transition = 0;
 	MoveCameraArroundObject();
 	virtual ~MoveCameraArroundObject();
 	
@@ -22,6 +30,10 @@ ALIEN_FACTORY MoveCameraArroundObject* CreateMoveCameraArroundObject() {
 	MoveCameraArroundObject* alien = new MoveCameraArroundObject();
 	// To show in inspector here
 	SHOW_IN_INSPECTOR_AS_INPUT_FLOAT(alien->speed);
+	SHOW_IN_INSPECTOR_AS_INPUT_FLOAT(alien->transition_duration);
+
 	SHOW_IN_INSPECTOR_AS_GAMEOBJECT(alien->path);
+	SHOW_IN_INSPECTOR_AS_GAMEOBJECT(alien->target);
+
 	return alien;
 } 
