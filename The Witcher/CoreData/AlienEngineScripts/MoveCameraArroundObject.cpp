75cc381f7e7962b2e9889e514b4811fd818dab03
Yessica Servin Dominguez <36170847+YessicaSD@users.noreply.github.com>
2020-05-26 16:41:44 +0200

    going back camera movement

--- The Witcher/CoreData/AlienEngineScripts/MoveCameraArroundObject.cpp
+++ The Witcher/CoreData/AlienEngineScripts/MoveCameraArroundObject.cpp
@@ -11,6 +11,7 @@ MoveCameraArroundObject::~MoveCameraArroundObject()
 void MoveCameraArroundObject::Start()
 {
 	start_pos = game_object->transform->GetGlobalPosition();
+	
 	if (path)
 	{
 		curve = path->GetComponent<ComponentCurve>();
@@ -18,7 +19,8 @@ void MoveCameraArroundObject::Start()
 		{
 			LOG("Curve found!");
 			camera = GetComponent<ComponentCamera>();
-			start_curve = curve->curve.ValueAt(current_pos);
+			start_curve = curve->curve.ValueAt(0);
+			end_curve = curve->curve.ValueAt(1);
 		}
 
 	}
@@ -26,7 +28,7 @@ void MoveCameraArroundObject::Start()
 
 void MoveCameraArroundObject::Update()
 {
-	if (curve && camera)
+	if (!ended_intro && curve && camera)
 	{
 		float3 currentPos = target->transform->GetGlobalPosition();
 
@@ -35,14 +37,25 @@ void MoveCameraArroundObject::Update()
 			current_time_transition += Time::GetDT();
 			if (current_time_transition >= transition_duration)
 			{
-				currentPos = math::Lerp(start_pos, start_curve, 1);
 				start_transition = false;
+				current_time_transition = transition_duration;
 			}
-			else
+
+			float percentatge_lerp = current_time_transition / transition_duration;
+			currentPos = math::Lerp(start_pos, start_curve, percentatge_lerp);
+		}
+		else if (go_back)
+		{
+			current_time_transition += Time::GetDT();
+			if (current_time_transition >= transition_duration)
 			{
-				float percentatge_lerp = current_time_transition / transition_duration;
-				currentPos = math::Lerp(start_pos, start_curve, percentatge_lerp);
+				current_time_transition = transition_duration;
+				go_back = false;
+				ended_intro = true;
 			}
+
+			float percentatge_lerp = current_time_transition / transition_duration;
+			currentPos = math::Lerp(end_curve, start_pos, percentatge_lerp);
 		}
 		else
 		{
@@ -50,7 +63,8 @@ void MoveCameraArroundObject::Update()
 			current_pos += speed * Time::GetDT();
 			if (current_pos > 1)
 			{
-				current_pos = 0;
+				go_back = true;
+				current_time_transition = 0;
 			}
 		}
 
