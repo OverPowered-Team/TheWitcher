16507b1093231acac7dfb1786f7ef7efb950520d
Alex Morales Garcia <36154498+AlexMG99@users.noreply.github.com>
2020-05-28 16:29:17 +0200

    Added can get interrupted

--- The Witcher/CoreData/AlienEngineScripts/Enemy.h
+++ The Witcher/CoreData/AlienEngineScripts/Enemy.h
@@ -56,6 +56,8 @@ public:
 	virtual void OnTriggerEnter(ComponentCollider* collider) {};
 	virtual void OnDeathHit() {}
 
+	virtual void CanGetInterrupted();
+
 	virtual float GetDamaged(float dmg, PlayerController* player, float3 knock_back = float3::zero());
 	virtual float GetDamaged(float dmg, float3 knock_back = float3::zero());
 	void AddEffect(Effect* new_effect);
