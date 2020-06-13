8cb1e41307cab73fd1a7aac337ee7f148616b821
AxelAlavedra <axel.alavedra@gmail.com>
2020-06-13 10:03:44 +0200

    Update FootTrigger.cpp

--- The Witcher/CoreData/AlienEngineScripts/FootTrigger.cpp
+++ The Witcher/CoreData/AlienEngineScripts/FootTrigger.cpp
@@ -20,18 +20,8 @@ void FootTrigger::Start()
 
 void FootTrigger::Update()
 {
-	/*std::vector<ComponentCollider*> colliders = Physics::OverlapSphere(transform->GetGlobalPosition(), 0.5);
-	for (auto i = colliders.begin(); i != colliders.end(); ++i)
-	{
-		SoundMaterial* sound_mat = (*i)->game_object_attached->GetComponent<SoundMaterial>();
-		if (sound_mat && player)
-		{
-			//player->OnTerrainEnter(sound_mat->GetMaterialName());
-			player->audio->SetSwitchState("Material", sound_mat->GetMaterialName().c_str());
-			player->audio->StartSound("Player_Footstep");
-		}
-	}*/
-	rbdy->SetPosition(game_object->parent->transform->GetGlobalPosition());
+	if(rbdy)
+		rbdy->SetPosition(game_object->parent->transform->GetGlobalPosition());
 }
 
 void FootTrigger::OnTriggerEnter(ComponentCollider* collider)
@@ -39,7 +29,6 @@ void FootTrigger::OnTriggerEnter(ComponentCollider* collider)
 	SoundMaterial* sound_mat = collider->game_object_attached->GetComponent<SoundMaterial>();
 	if (sound_mat && player)
 	{
-		LOG("TOUCH GROUND");
 		//player->OnTerrainEnter(sound_mat->GetMaterialName());
 		player->audio->SetSwitchState("Material", sound_mat->GetMaterialName().c_str());
 		player->audio->StartSound("Player_Footstep");
