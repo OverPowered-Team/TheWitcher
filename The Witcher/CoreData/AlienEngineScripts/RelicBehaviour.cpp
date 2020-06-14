6e728167566561f8e97672e4c30e4966e11f5159
CereVenteo <36203330+CereVenteo@users.noreply.github.com>
2020-06-14 03:20:10 +0200

    bugfix audios

--- The Witcher/CoreData/AlienEngineScripts/RelicBehaviour.cpp
+++ The Witcher/CoreData/AlienEngineScripts/RelicBehaviour.cpp
@@ -103,6 +103,7 @@ void DashRelic::OnPickUp(PlayerController* _player, std::string attack)
 	DashEffect* effect = new DashEffect();
 	effect->on_dash_effect = effect_to_apply;
 	effect->name = effect_to_apply->name;
+	effect->sound_name = sound_name;
 
 	switch (relic_effect)
 	{
@@ -269,12 +270,13 @@ void RelicBehaviour::SetRelic(const char* json_array)
 		else if (std::strcmp(json_array, "DASH") == 0)
 		{
 			EffectData* _effect = new EffectData();
+			((DashRelic*)relic)->sound_name = type_array->GetString("dash_effect.sound_name");
 			_effect->name = type_array->GetString("dash_effect.name");
 			_effect->vfx_on_apply = type_array->GetString("dash_effect.vfx_on_apply");
 			_effect->vfx_position = type_array->GetNumber("dash_effect.vfx_position");
 			_effect->time = type_array->GetNumber("dash_effect.time");
 			_effect->ticks_time = type_array->GetNumber("dash_effect.ticks_time");
-
+			
 			JSONArraypack* flat_modifiers = type_array->GetArray("dash_effect.flat_modifiers");
 			if (flat_modifiers)
 			{
@@ -302,7 +304,7 @@ void RelicBehaviour::SetRelic(const char* json_array)
 					mult_modifiers->GetAnotherNode();
 				}
 			}
-
+			
 			((DashRelic*)relic)->effect_to_apply = _effect;
 		}
 	}
